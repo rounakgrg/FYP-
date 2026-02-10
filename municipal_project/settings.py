e50c181e77af36c1d071ef65b7edc5192aacef2b
Rounak <Rounak@DESKTOP-6ACDFAQ>
2026-02-10 16:04:48 +0545

    Fix: Correct static files configuration for production

--- municipal_project/settings.py
+++ municipal_project/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
