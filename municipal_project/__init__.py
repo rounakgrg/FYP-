96cd026090ec9b539c50c9d4fd85e9f4ecc0eb9f
Rounak <Rounak@DESKTOP-6ACDFAQ>
2026-02-10 13:23:09 +0545

    Fix Netlify build: monkeypatch mysqlclient version for Django

--- municipal_project/__init__.py
+++ municipal_project/__init__.py
@@ -1,3 +1,8 @@
 import pymysql
 
 pymysql.install_as_MySQLdb()
+import MySQLdb
+
+# Monkeypatch version to satisfy Django
+MySQLdb.version_info = (2, 2, 7, 'final', 0)
+MySQLdb.__version__ = '2.2.7'
