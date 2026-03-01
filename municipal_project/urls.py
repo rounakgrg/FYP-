7f00d74f16f623b5feaa1ba5fc15e73b2b5d21b9
Rounak <Rounak@DESKTOP-6ACDFAQ>
2026-03-01 10:34:15 +0545

    Fix media URLs for production (DEBUG=False) and restore settings import

--- municipal_project/urls.py
+++ municipal_project/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,4 +29,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+else:
+    # Serve media files in production (Render) where DEBUG=False
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
